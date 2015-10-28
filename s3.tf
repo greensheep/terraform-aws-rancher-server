5608e602f2c9f729c9f1c8576370fc5f5eb5b181
Tom Hill <tom@greensheep.co.uk>
2015-10-28 13:16:24 +0000

    Switched to server_hostname var for naming bucket

--- s3.tf
+++ s3.tf
@@ -1,12 +1,12 @@
 # S3 bucket to hold rancher server api credentials
 resource "aws_s3_bucket" "server_credentials_bucket" {
 
-    bucket = "${var.server_name}-credentials"
+    bucket = "${var.server_hostname}-credentials"
     acl = "private"
     force_destroy = true
 
     tags {
-        Name = "${var.server_name}"
+        Name = "${var.server_hostname}"
         ManagedBy = "terraform"
     }
 
