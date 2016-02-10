b8dc6848b417f3b4b8be85df88f763fa338e6ea6
Tom Hill <tom@greensheep.co.uk>
2016-02-10 12:57:51 +0000

    Allow docker api/SSH egress to anywhere, create standalone security group rules

--- security-groups.tf
+++ security-groups.tf
@@ -5,71 +5,125 @@ resource "aws_security_group" "rancher_server_sg" {
     description = "Rules for the Rancher server instance."
     vpc_id = "${var.vpc_id}"
 
-    # Incoming HTTP from internet
-    # Nginx will redirect all http traffic to https
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    # Incoming HTTPS from internet
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    # Outgoing HTTP to internet
-    egress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    # Outgoing HTTPS to internet
-    egress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    # Outgoing SSH to VPC private subnets
-    egress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [
-            "${split(",",var.vpc_private_subnets)}"
-        ]
-    }
-
-    # Outgoing docker API to VPC private subnets
-    egress {
-        from_port = 2376
-        to_port = 2376
-        protocol = "tcp"
-        cidr_blocks = [
-            "${split(",",var.vpc_private_subnets)}"
-        ]
-    }
-
-    # Outgoing mysql
-    egress {
-        from_port = 3306
-        to_port = 3306
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
     tags {
         Name = "${var.server_name}"
         ManagedBy = "terraform"
     }
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+}
+
+# Incoming HTTP from anywhere
+# Nginx will redirect all http traffic to https
+resource "aws_security_group_rule" "http_ingress" {
+
+    security_group_id = "${aws_security_group.rancher_server_sg.id}"
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+}
+
+# Incoming HTTPS from anywhere
+resource "aws_security_group_rule" "https_ingress" {
+
+    security_group_id = "${aws_security_group.rancher_server_sg.id}"
+    type = "ingress"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+}
+
+# Outgoing HTTP to anywhere
+resource "aws_security_group_rule" "http_egress" {
+
+    security_group_id = "${aws_security_group.rancher_server_sg.id}"
+    type = "egress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+}
+
+# Outgoing HTTPS to anywhere
+resource "aws_security_group_rule" "https_egress" {
+
+    security_group_id = "${aws_security_group.rancher_server_sg.id}"
+    type = "egress"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+}
+
+# Outgoing SSH to anywhere
+# Allows configuration of rancher host instances via the UI
+resource "aws_security_group_rule" "ssh_egress" {
+
+    security_group_id = "${aws_security_group.rancher_server_sg.id}"
+    type = "egress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+}
+
+# Outgoing docker API to anywhere
+# Allows configuration of rancher host instances via the UI
+resource "aws_security_group_rule" "docker_egress" {
+
+    security_group_id = "${aws_security_group.rancher_server_sg.id}"
+    type = "egress"
+    from_port = 2376
+    to_port = 2376
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+}
+
+# Outgoing mysql
+resource "aws_security_group_rule" "mysql_egress" {
+
+    security_group_id = "${aws_security_group.rancher_server_sg.id}"
+    type = "egress"
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
     lifecycle {
         create_before_destroy = true
